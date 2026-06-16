c39c1ebbcf76d7c3d37365b2704fbe829dab3ca7
Kunal Yadav <kunalyadav@Kunals-MacBook-Air.local>
2026-06-16 11:28:52 +0530

    fixing loader in shorts

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
     <div className="flex flex-col items-center justify-center gap-3">
-      <div className="w-12 h-12 rounded-full border-4 border-gray-200 border-t-blue-500 animate-spin" />
+      <div className="w-12 h-12 rounded-full border-4 border-gray-200 border-t-red-500 animate-spin" />
     </div>
   );
 }
